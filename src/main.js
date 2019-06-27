6c6bc5bb93012b9e125a790956599d17b55beaa1
melizasosa <s.m.meliza@gmail.com>
2019-06-27 13:05:41 -0500

    validacion de usuario

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
 /* Manejo del DOM */
+
+//VARIABLES
+const user=document.getElementById("user");
+const password=document.getElementById("password");
+const msgFail=document.getElementById("msgFail");
+let cont=0;
+
+
+//BOTONES
+const buttonLogin=document.getElementById("buttonLogin");
+
+//FUNCIONES
+buttonLogin.addEventListener ("click",()=>{
+
+const userLogin=user.value;
+const passwordLogin=password.value;
+
+    if(userLogin==="LABORATORIA" && passwordLogin==="LABORATORIA"){
+        
+        console.log("validando condicion");
+        console.log("la clave es correcto");
+        cont=0;
+         }
+        else if(cont>=2){
+        console.log("bloqueo");
+        document.getElementById('user').disabled = true;
+        document.getElementById('password').disabled = true;
+        document.getElementById('buttonLogin').disabled = true;
+        }
+    else{
+        cont=cont+1;
+        console.log("ERROR");
+        msgFail.innerHTML="Clave incorrecta";
+        }
+});
+
