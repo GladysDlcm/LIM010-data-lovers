c86244d1b59b8189f795be3ccc11e213558fddfe
melizasosa <s.m.meliza@gmail.com>
2019-06-28 02:15:35 -0500

    Validacion de contraseña close #9

--- src/main.js
+++ src/main.js
@@ -14,28 +14,32 @@ const buttonLogin=document.getElementById("buttonLogin");
 //FUNCIONES
 buttonLogin.addEventListener ("click",(event)=>{
 event.preventDefault();
-console.log("hola");
+
 const userLogin=user.value;
 const passwordLogin=password.value;
 
     if(userLogin==="LABORATORIA" && passwordLogin==="LABORATORIA"){
-        
-        console.log("validando condicion");
-        console.log("la clave es correcto");
+     
         viewPokemon.classList.add("show");
         viewLogin1.classList.add("hide");
         cont=0;
          }
         else if(cont>=2){
         console.log("bloqueo");
+        document.getElementById("user").value=""
+        document.getElementById("password").value=""
+        msgFail.classList.add("hide");
         document.getElementById('user').disabled = true;
         document.getElementById('password').disabled = true;
         document.getElementById('buttonLogin').disabled = true;
+        alert("Superó los numeros de intentos, vuelva a cargar!");
         }
     else{
         cont=cont+1;
         console.log("CLAVE INCORRECTA");
         msgFail.innerHTML="Clave incorrecta";
+        document.getElementById("user").value=""
+        document.getElementById("password").value=""
         }
 });
 
